a4733d2f208081fc13f2743328179aa13e35b0fa
v.anyanwu <victoruanyanwu@gmail.com>
2021-06-24 14:06:32 -0400

    minor text edit

--- push.js
+++ push.js
@@ -1,4 +1,5 @@
 var push = require('web-push'); 
+
 const vapidkeys = {   
     publicKey: 'BCXlSJK_T5VYzNkm-LaqXtxjdTi4cqGKYMURQfCQMbnwZNacsjukA5SYQj1ipAVDAdx31cNpDaJ_BUIMfjxhgjA',       
     privateKey: '2YZo94HU98QxWY3XrymOZphkjYUH_n36pjMwjnYzcX8'
@@ -10,8 +11,13 @@ const vapidkeys = {
   }
   //This would be gotten from our database of users who have allowed 
   // notification access
-const sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/d8H88AQo1pM:APA91bGCfDSesrq_OEze0Q0Ka13VEEAu5Q_wYLbMI2JSjyHSdRwp7opvMuIOYJmm2IellDFV5AW1Un4bAAeq913ECHfI19z7136RqWqMesg_Gi_KbrELQzzcB2yXV_HPzjnd4ZVWS49J", "expirationTime": null, "keys": { "p256dh": "BM8ny5YKiyLyMAFgbswGk-zhTOKS7zIir6ICf2soKtYq7mD812IkInDXzwUBVL6CLfpOyikzFA8cnBk-kNPxqgQ", "auth": "Hz83VyJPiFfJpblN9LlRGg" } }
+const sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/d8H88AQo1pM:APA91bGCfDSesrq_OEze0Q0Ka13VEEAu5Q_wYLbMI2JSjyHSdRwp7opvMuIOYJmm2IellDFV5AW1Un4bAAeq913ECHfI19z7136RqWqMesg_Gi_KbrELQzzcB2yXV_HPzjnd4ZVWS49J",
+              "expirationTime": null, 
+              "keys": { "p256dh": "BM8ny5YKiyLyMAFgbswGk-zhTOKS7zIir6ICf2soKtYq7mD812IkInDXzwUBVL6CLfpOyikzFA8cnBk-kNPxqgQ",
+                        "auth": "Hz83VyJPiFfJpblN9LlRGg" } }; 
 
-  push.sendNotification(sub, 'test message').then(console.log("Notif sent successfully")).catch(err => {console.log("That didnt work", err)}); 
+  push.sendNotification(sub, 'test message')
+  .then(console.log("Notif sent successfully"))
+  .catch(err => {console.log("That didnt work", err)}); 
 
 
